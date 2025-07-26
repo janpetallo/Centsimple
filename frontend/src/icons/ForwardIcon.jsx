bb83778f99e350f647353b91b38a9e2f936a915c
janpetallo <101074771+janpetallo@users.noreply.github.com>
2025-07-25 20:26:57 -0600

    style(ui): apply material 3 design to pagination

---
+++ frontend/src/icons/ForwardIcon.jsx
@@ -0,0 +1,15 @@
+function ForwardIcon({ className = '', ...props }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      fill="currentColor"
+      className={className}
+      {...props}
+    >
+      <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+    </svg>
+  );
+}
+
+export default ForwardIcon;
